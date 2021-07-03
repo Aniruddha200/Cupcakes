ca43c13dcb78ba27c4a5b9ca8d6ed0483bbbf1ad
Aniruddha <aniruddha200baidya@protonmail.com>
2021-07-03 06:06:16 +0100

    Initial Commit

---
+++ Cupcakes/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Cupcakes
+//
+//  Created by administrator on 03/07/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Cupcakes/CupcakesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakesApp.swift
+//  Cupcakes
+//
+//  Created by administrator on 03/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct CupcakesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
